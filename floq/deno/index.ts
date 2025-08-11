edba6418657c15f69dbdbb636b4a2278111883ac
knack <knack@knackcoffeebar.com>
2025-08-12 00:14:50 +0300

    added floq.json to track configs

--- floq/deno/index.ts
+++ floq/deno/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello from Deno");
+import { OpenAPIHono } from "@hono/zod-openapi";
+// import scheduler
+import { logger } from "hono/logger";
+import { cors } from "hono/cors";
+
+export function startApp() {
+	const rootApp = new OpenAPIHono();
+	// Global middleware
+	rootApp
+		.use(
+			cors({
+				origin: "*",
+			})
+		)
+		.use(logger());
+
+	Deno.serve(rootApp.fetch);
+}
