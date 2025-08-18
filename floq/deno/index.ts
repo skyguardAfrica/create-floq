719c699f709ff4e4e188de5dc333920bc2251a95
knack <knack@knackcoffeebar.com>
2025-08-18 13:23:06 +0300

    bumped floq version and updated starter file interface

--- floq/deno/index.ts
+++ floq/deno/index.ts
@@ -1,18 +1,42 @@
+import process from "node:process";
 import { OpenAPIHono } from "@hono/zod-openapi";
-// import scheduler
 import { logger } from "hono/logger";
 import { cors } from "hono/cors";
+import tasks from "../src/main.ts";
+import { createRoute } from "@floq/floq";
+import scheduler from "@floq/scheduler";
 
 if (import.meta.main) {
-	const rootApp = new OpenAPIHono();
-	// Global middleware
-	rootApp
-		.use(
-			cors({
-				origin: "*",
-			})
-		)
-		.use(logger());
+  const rootApp = new OpenAPIHono();
+  // Global middleware
+  rootApp
+    .use(
+      cors({
+        origin: "*",
+      })
+    )
+    .use(logger());
 
-	Deno.serve(rootApp.fetch);
+  // Add app route
+  const clientApp = createRoute(tasks);
+  rootApp.route("/", clientApp);
+
+  scheduler.startScheduler();
+  const server = Deno.serve(rootApp.fetch);
+
+  // graceful shutdown
+  process.on('SIGINT', () => {
+    server.shutdown();
+    process.exit(0)
+  })
+  process.on('SIGTERM', () => {
+    server.shutdown()
+      .then(() => process.exit(0))
+      .catch((err) => {
+        if (err) {
+          console.error(err)
+          process.exit(1)
+        }
+      })
+  })
 }
